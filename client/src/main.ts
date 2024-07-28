523007277d0f4d42ae4056b6c86f63901eda46ff
ziad-ezzat <ezzatziad83@gmail.com>
2024-07-28 22:52:11 +0200

    change tech

--- client/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { MyComponentZiadComponent } from './app/my-component-ziad/my-component-ziad.component';
-
-bootstrapApplication(MyComponentZiadComponent, appConfig)
-  .catch((err) => console.error(err));
